b05eb3ba1fce31dc4c15f08fcf21a3b928c0948d
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2020-04-26 22:09:10 +0300

    Add nested when to tests

--- test/clj_kondo/missing_body_in_when_test.clj
+++ test/clj_kondo/missing_body_in_when_test.clj
@@ -8,15 +8,22 @@
     (assert-submaps
      '({:file "<stdin>", :row 1, :col 1, :level :warning,
         :message "Missing body in when expression"})
-     (lint! "(when true)")))
+     (lint! "(when (> 1 0))")))
   (testing "test linting error of missing body in when for cljs"
     (assert-submaps
      '({:file "<stdin>", :row 1, :col 1, :level :warning,
         :message "Missing body in when expression"})
-     (lint! "(when true)" "--lang" "cljs"))))
+     (lint! "(when true)" "--lang" "cljs")))
+  (testing "test linting error of missing body in nested when"
+    (assert-submaps
+     '({:file "<stdin>", :row 1, :col 15, :level :warning,
+        :message "Missing body in when expression"})
+     (lint! "(when (> 1 0) (when (= 1 2)))"))))
 
 (deftest missing-body-in-when-valid-test
   (testing "test linting when with condition and body"
-    (is (empty? (lint! "(when true (prn 1))"))))
+    (is (empty? (lint! "(when (> 1 0) (prn 1))"))))
   (testing "test linting when with multiple expresion in body"
-    (is (empty? (lint! "(when true (prn 1) (+ 1 1))")))))
+    (is (empty? (lint! "(when true (prn 1) (+ 1 1))"))))
+  (testing "test linting nested when"
+    (is (empty? (lint! "(when (> 1 0) (when (prn 1) (+ 1 1)))")))))
